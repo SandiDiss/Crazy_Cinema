00c911f588b6ab3e32c5ea3af5fb624130ba2edf
SandiDiss <60228620+SandiDiss@users.noreply.github.com>
2020-01-28 00:29:47 +0530

    Update MainActivity.java

--- app/src/main/java/com/sandali/crazy_cinema/views/MainActivity.java
+++ app/src/main/java/com/sandali/crazy_cinema/views/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         new Handler().postDelayed(new Runnable() {
+            //goes to the login activity
             @Override
             public void run() {
                 Intent homeIntent = new Intent(MainActivity.this, LoginActivity.class);
